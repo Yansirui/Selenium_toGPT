9ff7af5a6abb10749290533ef9ada332a31341ca
Yansirui <118953128+Yansirui@users.noreply.github.com>
2024-01-03 13:10:02 +0800

    Update testing_apis.py

--- testing_apis.py
+++ testing_apis.py
@@ -1,27 +1,40 @@
-import api_backend
+#import api_backend
+import json
 
-#api_backend.start_chat_gpt()
-with open(r'Dataset/Corpus/term-timesPair.txt','r',encoding='utf-8') as f:
-    termpairs = f.readlines()[6200:]
-terms=[]
-response=[]
-for t in termpairs:
-    terms.append(t.split('\t')[0])
-chunk_size=20
-result_list = [terms[i:i + chunk_size] for i in range(0, len(terms), chunk_size)]
-times=0
-with open(r'Dataset/Corpus/responses_fromgpt.txt','a',encoding='utf-8') as f:
-    for terms_list in result_list:
-        if times==0:
-            api_backend.start_chat_gpt()
-        times=times+1
-        prompt = r'我将提供给你一个包含20个中文词汇的列表，请你对列表中的中文词汇进行分类，中文词汇列表为{},具体类别为[通用领域词汇，电商领域词汇，医疗领域词汇，金融领域词汇，文娱领域词汇]，反馈格式为json格式，如 "可以":"通用领域词汇" ，“电影”:"文娱领域词汇"'.format(terms_list)
-        a = api_backend.make_gpt_request(prompt)
-        receive,q=api_backend.Receive_limit()
-        if receive:
-            a=q
-        a=a.replace('\n','')
-        response.append(a)
-        print(a)
-        f.write(a+'\n')
-api_backend.stop_chat_gpt()
+def read_json(json_path):
+    with open(json_path,'r',encoding='utf-8') as f:
+        str=f.read()
+        data=json.loads(str)
+        return data
+def get_information(content):
+    #print(sentences[373])
+    #sentences=list(set(sentences))
+    #print(len(sentences))
+    times=0
+    with open(r'D:/Postgratuate/项目/继续预训练框架/构建sts数据集/医疗领域/responses_fromgpt.txt','a',encoding='utf-8') as f:
+        for term_sentence in content:
+            if times!= 0 and times % 150 == 0:
+                api_backend.add_session()
+            if times==0:
+                api_backend.start_chat_gpt()
+            times=times+1
+            prompt = r'假设你是一个医生，请你根据"{}"这句话对{}解释的角度，结合自己的知识，对这句话进行精简改写'.format(term_sentence['description'],term_sentence['term'])
+            a = api_backend.make_gpt_request(prompt)
+            #receive,q=api_backend.Receive_limit()
+            #if receive:
+            #    a=q
+            a=a.replace('\n','')
+            print(a)
+            f.write(a+'\n')
+    api_backend.stop_chat_gpt()
+
+while True:
+    with open(r'D:/Postgratuate/项目/继续预训练框架/构建sts数据集/医疗领域/responses_fromgpt.txt','r',encoding='utf-8') as f:
+        sentences = f.readlines()
+    content = read_json(r'D:/Postgratuate/项目/继续预训练框架/构建sts数据集/医疗领域/term_descriptions.json')[len(sentences):]
+    if len(content) == 0 :
+        break
+    else:
+        import api_backend
+        print(content[0]['description'])
+        get_information(content)
